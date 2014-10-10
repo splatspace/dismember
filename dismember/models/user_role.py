98a29fbe605a9ee982a8fa4988c3b8f0c4fcf801
Shaw Terwilliger <sterwill@tinfig.com>
2014-10-10 15:54:20 -0400

    Put a unique index on users_roles

--- dismember/models/user_role.py
+++ dismember/models/user_role.py
@@ -1,4 +1,4 @@
-from peewee import ForeignKeyField, CompositeKey
+from peewee import ForeignKeyField
 from dismember.models.role import Role
 from dismember.models.user import User
 
@@ -10,13 +10,19 @@ class UserRole(db.Model):
 
     class Meta:
         db_table = 'users_roles'
-        # Defining this primary key trips up Flask-Security's Peewee datastore when
-        # it creates users with roles.  Leaving it out for now.
+
+        # Defining this as a primary key trips up Flask-Security's Peewee datastore when
+        # it creates users with roles.  Using a plain old unique index works, though.
         # primary_key = CompositeKey('user', 'role')
+
+        indexes = (
+            (('role', 'user'), True),
+        )
 
     user = ForeignKeyField(User, related_name='roles')
     role = ForeignKeyField(Role, related_name='users')
     name = property(lambda self: self.role.name)
     description = property(lambda self: self.role.description)
+
 
 UserRole.create_table(fail_silently=True)
