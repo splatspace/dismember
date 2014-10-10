baba566490bd8febea919e46cad7b629da3b2c68
Shaw Terwilliger <sterwill@tinfig.com>
2014-10-10 10:34:19 -0400

    Removing old Flask cruft

--- dismember/service.py
+++ dismember/service.py
@@ -3,22 +3,19 @@ from flask.ext.peewee.auth import Auth
 
 import os
 from flask import Flask
-from flask.ext.assets import Environment
 from itsdangerous import URLSafeTimedSerializer
 from flask_peewee.db import Database
 
 
 FLASK_APP_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Don't import any modules that use DB models up here; import them as required
-# but after db.configure_db has been executed.  This file is the only file that
-# requires this special treatment.  Other files can import and use models freely.
+# Don't import any modules that use DB models up here, because they need to
+# import and use the "db" object from this module (which would not be initialized
+# yet).  Instead, import them as requred after db has been initialized.  This
+# file is the only file that requires this special treatment.  Other files can
+# import and use models freely.
 
 app = Flask(__name__)
-
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-# login_manager.login_view = '/members/login'
 
 # Loaded by run
 base = None
@@ -83,45 +80,6 @@ def run(the_config):
     import dismember.admin
 
     admin.setup()
-
-    # Configure Flask-Mail
-    # mail = Mail(app)
-    # app.mail = mail
-
-    # Flask-Security Core
-    # app.config['SECURITY_URL_PREFIX'] = '/security'
-    # app.config['SECURITY_PASSWORD_HASH'] = 'pbkdf2_sha512'
-    # app.config['SECURITY_PASSWORD_SALT'] = config['flask']['password_salt']
-
-    # Flask-Security Feature Flags
-    # app.config['SECURITY_CONFIRMABLE'] = True
-    # app.config['SECURITY_REGISTERABLE'] = True
-    # app.config['SECURITY_RECOVERABLE'] = True
-    # app.config['SECURITY_TRACKABLE'] = True
-    # app.config['SECURITY_CHANGEABLE'] = True
-
-    # Flask-Login
-    # app.config['REMEMBER_COOKIE_DURATION'] = config['flask']['remember_me_cookie_duration']
-
-    # Flask-Email
-    # app.config['MAIL_SERVER'] = 'localhost'
-    # app.config['MAIL_PORT'] = 25
-    # app.config['MAIL_USE_SSL'] = True
-    # app.config['MAIL_USERNAME'] = 'username'
-    # app.config['MAIL_PASSWORD'] = 'password'
-
-    # Flask-Security Core
-    #user_datastore = SQLAlchemyUserDatastore(db, dismember.models.user.User, dismember.models.role.Role)
-    #security = Security(app, user_datastore)
-
-    # Flask-Admin
-    # admin = Admin(app, name=config['site']['name'])
-
-    # Flask-Assets
-    assets = Environment(app)
-    assets_output_dir = os.path.join(FLASK_APP_DIR, 'static', 'gen')
-    if not os.path.exists(assets_output_dir):
-        os.mkdir(assets_output_dir)
 
     # Import the views to enable Flask handlers
     import dismember.views
