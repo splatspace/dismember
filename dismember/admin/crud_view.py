c05609eeb6072f2681d3b7601cf9e0f82eedd6e6
Shaw Terwilliger <sterwill@tinfig.com>
2015-01-08 09:22:15 -0500

    item_view -> crud_view

---
+++ dismember/admin/crud_view.py
@@ -0,0 +1,116 @@
+from string import capitalize
+
+from dismember.service import db
+from flask import render_template, request, flash, redirect, url_for
+
+
+def configure_crud_view(blueprint, name, item_cls, new_item_form_cls, edit_item_form_cls, item_name_singluar='Item',
+                        item_name_plural='Items', list_order_by=None):
+    """
+    Configures a simple create/read/update/delete (CRUD) view for a SQLAlchemy model item.
+
+    :param blueprint: the blueprint to attach the views to
+    :param name: the name of the item to use in the blueprint to host the views.  If the blueprint's name is
+        "admin" and this parameter is "users", the resulting view's URLs will end like ".../admin/users/new",
+        ".../admin/users/123/delete", etc.
+    :param item_cls: the SQLAlchemy model class being created/read/updated/deleted
+    :param new_item_form_cls: the WTForms-Alchemy form class to show when creating a new item
+    :param edit_item_form_cls: the WTForms-Alchemy form class to show when reading and updating a new item
+    :param item_name_singluar: a string used in the UI when a single item is described ('user', 'car', 'ox')
+    :param item_name_plural: a string used in the UI when multiple items are described ('users', 'cars', 'oxen')
+    :param list_order_by: a SQLAlchemy order_by clause to apply when listing items
+    """
+
+    if '.' in name:
+        raise ValueError('Item name cannot contain a period')
+
+    # Compute the endpoint names for this item so they can be used inside templates to link
+    # to other CRUD endpoints and for redirection.
+    pfx = '%s.%s_' % (blueprint.name, name)
+    endpoints = dict(
+        list_endpoint=pfx + 'list',
+        create_endpoint=pfx + 'create',
+        new_endpoint=pfx + 'new',
+        view_endpoint=pfx + 'view',
+        update_endpoint=pfx + 'update',
+        delete_endpoint=pfx + 'delete',
+    )
+
+    def list_items():
+        """Renders a list of all items."""
+        q = item_cls.query
+        if list_order_by:
+            q = q.order_by(list_order_by)
+        items = q.all()
+        return render_template('items/list.html',
+                               items=items,
+                               title=item_name_plural,
+                               **endpoints)
+
+    def view_item(item_id):
+        """Renders a form that lets users view and edit item data."""
+        item = item_cls.query.get_or_404(item_id)
+        form = edit_item_form_cls(obj=item)
+        return render_template('items/view.html',
+                               form=form,
+                               item=item,
+                               title=str(item),
+                               **endpoints)
+
+    def update_item(item_id):
+        """Processes the view item form data and updates an existing item."""
+        item = item_cls.query.get_or_404(item_id)
+        form = edit_item_form_cls(request.form)
+        form.populate_obj(item)
+        db.session.add(item)
+        db.session.commit()
+        flash('%s "%s" updated' % (capitalize(item_name_singluar), str(item)))
+        return redirect(url_for(endpoints['view_endpoint'], item_id=item_id))
+
+    def create_item():
+        """Processes the new item form data and creates an item."""
+        item = item_cls()
+        form = new_item_form_cls(request.form)
+        form.populate_obj(item)
+        db.session.add(item)
+        db.session.commit()
+        flash('%s "%s" created' % (capitalize(item_name_singluar), str(item)))
+        return redirect(url_for(endpoints['list_endpoint']))
+
+    def new_item():
+        """Renders the new item form."""
+        form = new_item_form_cls()
+        return render_template('items/new.html',
+                               form=form,
+                               item=None,
+                               title='New %s' % capitalize(item_name_singluar),
+                               **endpoints)
+
+    def delete_item(item_id):
+        item = item_cls.query.get_or_404(item_id)
+        item_str = str(item)
+        db.session.delete(item)
+        db.session.commit()
+        flash('%s "%s" deleted' % (capitalize(item_name_singluar), item_str))
+        return 'ok', 200
+
+    def short_endpoint(endpoint_name_key):
+        """
+        Get the part of the endpoint after the blueprint name (for registering URL rules
+        inside a blueprint).
+        """
+        return endpoints[endpoint_name_key].split('.', 2)[-1]
+
+    blueprint.add_url_rule('/%s' % name, short_endpoint('list_endpoint'),
+                           view_func=list_items, methods=['GET'])
+    blueprint.add_url_rule('/%s/new' % name, short_endpoint('new_endpoint'),
+                           view_func=new_item, methods=['GET'])
+    blueprint.add_url_rule('/%s/new' % name, short_endpoint('create_endpoint'),
+                           view_func=create_item, methods=['POST'])
+    blueprint.add_url_rule('/%s/<int:item_id>' % name, short_endpoint('view_endpoint'),
+                           view_func=view_item, methods=['GET'])
+    blueprint.add_url_rule('/%s/<int:item_id>' % name, short_endpoint('update_endpoint'),
+                           view_func=update_item, methods=['POST'])
+    blueprint.add_url_rule('/%s/<int:item_id>' % name, short_endpoint('delete_endpoint'),
+                           view_func=delete_item, methods=['DELETE'])
+
--- dismember/admin/users.py
+++ dismember/admin/users.py
@@ -1,4 +1,7 @@
+from string import capitalize
+
 from dismember.admin import admin_bp
+from dismember.admin.crud_view import configure_crud_view
 from dismember.models.user import User
 from dismember.service import db
 from dismember.wtforms_alchemy.forms import SessionModelForm, TimeZoneAwareFieldMeta
@@ -15,67 +18,4 @@ class NewUserForm(UserForm):
         exclude = ['confirmed_at', 'last_login_at', 'current_login_at', 'last_login_ip', 'current_login_ip',
                    'login_count']
 
-
-@admin_bp.route('/users')
-def users_list():
-    users = User.query.order_by(User.full_name.desc()).all()
-    return render_template('list_items.html',
-                           items=users,
-                           title='Users',
-                           new_endpoint='admin.users_new',
-                           view_endpoint='admin.users_view')
-
-
-@admin_bp.route('/users', methods=['POST'])
-def users_create():
-    user = User()
-    form = UserForm(request.form)
-    form.populate_obj(user)
-    db.session.add(user)
-    db.session.commit()
-    flash('User "%s" created' % str(user))
-    return redirect(url_for('admin.users_list'))
-
-
-@admin_bp.route('/users/new')
-def users_new():
-    form = NewUserForm()
-    return render_template('new_item.html',
-                           form=form,
-                           item=None,
-                           title='New User',
-                           create_endpoint='admin.users_create')
-
-
-@admin_bp.route('/users/<item_id>')
-def users_view(item_id):
-    user = User.query.get_or_404(item_id)
-    form = UserForm(obj=user)
-    return render_template('edit_item.html',
-                           form=form,
-                           item=user,
-                           title=str(user),
-                           update_endpoint='admin.users_update',
-                           delete_endpoint='admin.users_delete',
-                           after_delete_endpoint='admin.users_list')
-
-
-@admin_bp.route('/users/<item_id>', methods=['POST'])
-def users_update(item_id):
-    user = User.query.get_or_404(item_id)
-    form = UserForm(request.form)
-    form.populate_obj(user)
-    db.session.add(user)
-    db.session.commit()
-    flash('User "%s" updated' % str(user))
-    return redirect(url_for('admin.users_update', item_id=item_id))
-
-
-@admin_bp.route('/users/<item_id>/delete', methods=['DELETE'])
-def users_delete(item_id):
-    user = User.query.get_or_404(item_id)
-    user_str = str(user)
-    db.session.delete(user)
-    db.session.commit()
-    flash('User "%s" deleted' % user_str)
-    return 'ok', 200
+users_view = configure_crud_view(admin_bp, 'users', User, NewUserForm, UserForm, 'user', 'users', User.full_name)
